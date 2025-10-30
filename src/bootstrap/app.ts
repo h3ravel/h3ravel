e4b3ac18c5f12780ebb5302bdf7ca77f02b05ea1
3m1n3nc3 <mygames.ng@gmail.com>
2025-10-30 14:50:20 +0100

    feat: use single entry point to bootstrap app

--- src/bootstrap/app.ts
+++ src/bootstrap/app.ts
@@ -1,25 +1,17 @@
-import { Application, Kernel } from '@h3ravel/core'
-import { LogRequests, Request, Response } from '@h3ravel/http'
-
-import { HttpContext } from '@h3ravel/shared'
+import { h3ravel } from '@h3ravel/core'
 import providers from 'src/bootstrap/providers'
 
 export default class {
     async bootstrap () {
-        const app = new Application(process.cwd())
+        const app = await h3ravel(
+            providers,
+            process.cwd(),
+            {
+                autoload: true
+            },
+            async () => undefined
+        )
 
-        await app.quickStartup(providers)
-
-        const h3App = app.make('http.app')
-
-        const kernel = new Kernel((event) => HttpContext.init({
-            app,
-            request: new Request(event, app),
-            response: new Response(event, app)
-        }), [new LogRequests()])
-
-        h3App.use((event) => kernel.handle(event, async () => undefined))
-
-        await app.fire(h3App)
+        return await app.fire()
     }
 }
